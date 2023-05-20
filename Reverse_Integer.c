44274b3227d650978fc0049575dde74de799aae8
DhatriManyala <119917907+DhatriManyala@users.noreply.github.com>
2023-05-20 15:54:27 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int flag=0;     if (a<0)     {         flag=1;         a*=-1;     }     int rev=0,r;     while (a>0)     {         r=a%10;         rev=(rev*10)+r;         a=a/10;     }     if (flag==1)     {         printf("%d",rev*-1);     }     else     {         printf("%d",rev);     }     return 0; }

--- Reverse_Integer.c
+++ Reverse_Integer.c
@@ -1,14 +1,28 @@
 #include<stdio.h>
 int main()
 {
-    int n,q,r,s=0;
-    scanf("%d",&n);
-    q=n;
-    while(q!=0)
+    int a;
+    scanf("%d",&a);
+    int flag=0;
+    if (a<0)
     {
-        r=q%10;
-        s=s*10+r;
-        q=q/10;
+        flag=1;
+        a*=-1;
     }
-    printf("%d",s);
+    int rev=0,r;
+    while (a>0)
+    {
+        r=a%10;
+        rev=(rev*10)+r;
+        a=a/10;
+    }
+    if (flag==1)
+    {
+        printf("%d",rev*-1);
+    }
+    else
+    {
+        printf("%d",rev);
+    }
+    return 0;
 }
