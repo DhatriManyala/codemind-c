c2b3f61bde0442c034e6b4e70fe25f602a603a0f
DhatriManyala <119917907+DhatriManyala@users.noreply.github.com>
2023-05-22 20:43:27 +0530

    #include<stdio.h> int main() { 	int N,i=1,count=0; 	scanf("%d",&N); 	for(i;i<=N;i++) 	{ 		int sum=0; 		if(N%i==0) 		{ 			int j=1; 			while(j<=i) 			{ 				if(i%j==0) 				{ 					sum++; 				} 				j++; 			} 			if(sum!=2) 			{ 				count++; 			} 		} 	} 	printf("%d",count); }

--- Count_of_the_non-prime_divisors_of_a_given_number.c
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -1,32 +1,27 @@
 #include<stdio.h>
-int prime(int num)
-{
-    int c=0,i;
-    for (i=1; i<=num; i++)
-    {
-        if (num%i==0)
-        {
-            c++;
-        }
-    }
-    if (c==2)
-    {
-        return 1;
-    }
-    return 0;
-}
 int main()
 {
-    int a;
-    scanf("%d",&a);
-    int c=0,i;
-    for (i=1; i<a; i++)
-    {
-        if (a%i==0 && prime(i)==0)
-        {
-            c++;
-        }
-    }
-    printf("%d",c+1);
-    return 0;
+	int N,i=1,count=0;
+	scanf("%d",&N);
+	for(i;i<=N;i++)
+	{
+		int sum=0;
+		if(N%i==0)
+		{
+			int j=1;
+			while(j<=i)
+			{
+				if(i%j==0)
+				{
+					sum++;
+				}
+				j++;
+			}
+			if(sum!=2)
+			{
+				count++;
+			}
+		}
+	}
+	printf("%d",count);
 }
