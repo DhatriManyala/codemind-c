42792d07fe965f3e5c524db44034fb4e61ad3cc7
DhatriManyala <119917907+DhatriManyala@users.noreply.github.com>
2023-06-04 16:32:18 +0530

    #include<stdio.h> int main() {     float q,n,i,c=0;     scanf("%f",&n);     for(i=1;i<=n;i++)     {         q=n/i;         if(q==i)         {             printf("True");             c++;         }     }     if(c==0)     {         printf("False");     }     return 0; }

--- perfect_square_root_or_not.c
+++ perfect_square_root_or_not.c
@@ -1,23 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int c=0;
-    float a;
-    scanf("%f",&a);
-    for(int i=0;i<a/2;i++)
+    float q,n,i,c=0;
+    scanf("%f",&n);
+    for(i=1;i<=n;i++)
     {
-        if(i*i==a)
+        q=n/i;
+        if(q==i)
         {
+            printf("True");
             c++;
-            break;
         }
     }
-    if(c==1)
-    {
-        printf("True");
-    }
-    else
+    if(c==0)
     {
         printf("False");
     }
+    return 0;
 }
